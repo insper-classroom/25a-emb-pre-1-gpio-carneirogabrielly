7a2e81c5d7e579ec6073a5490f3fc8d5d06a22a3
Gabrielly <gabriellycs@al.insper.edu.br>
2025-03-10 17:00:11 -0300

    exercicio 3

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,43 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-int main() {
+const int BTN_RED = 28;
+const int BTN_GREEN = 26;
+const int LED_RED = 4;
+const int LED_GREEN = 6;
+
+int main()
+{
   stdio_init_all();
 
-  while (true) {
+  gpio_init(BTN_RED);
+  gpio_set_dir(BTN_RED, GPIO_IN);
+  gpio_pull_up(BTN_RED); // Ativando pull-up
+
+  gpio_init(BTN_GREEN);
+  gpio_set_dir(BTN_GREEN, GPIO_IN);
+  gpio_pull_up(BTN_GREEN); // Ativando pull-up
+
+  gpio_init(LED_RED);
+  gpio_set_dir(LED_RED, GPIO_OUT);
+  gpio_init(LED_GREEN);
+  gpio_set_dir(LED_GREEN, GPIO_OUT);
+
+  while (true)
+  {
+
+    if (!gpio_get(BTN_GREEN))
+    {
+      gpio_put(LED_GREEN, !gpio_get(LED_GREEN));
+      while (!gpio_get(BTN_GREEN)) {
+      };
+
+    }
+    if (!gpio_get(BTN_RED))
+    {
+      gpio_put(LED_RED, !gpio_get(LED_RED));
+      while (!gpio_get(BTN_RED)) {
+      };
+    }
   }
 }
